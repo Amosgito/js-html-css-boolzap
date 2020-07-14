7ec304416dd1923e6852c50a2a1ab695391f68e7
Davide Carpani <carpani.d@gmail.com>
2020-07-14 16:52:29 +0200

    js not working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
+
 $("#button").click(function() {
 
-  var txt = $("#txtinput").val()
+  var txt = $("#txtinput").val();
+  var template = $("#template-chat-1").clone();
 
-  $(".chat p").append("<p>" + txt + "</p>")
+  template.children(".template-chat-txt").text(txt); //...
+  // template.appendTo(".chat");
+  $(".chat").append(template);
+
+  setTimeout(risposta, 1000);
+
+  function risposta() {
+
+    var pctxt = "ok";
+    var pcTemplate = $("#template-chat-2").clone();
+    pcTemplate.children(".template-chat-txt-pc").text(pctxt);
+    $(".chat").append(pcTemplate);
+  }
 
 })
+
+var contactsrc = $("#contactsrc").val();
+var res = string.search(contactsrc);
+if (res == 1) {
+
+}
